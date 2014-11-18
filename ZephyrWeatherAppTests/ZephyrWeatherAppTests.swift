0708ea6ac7af24144ef87ba680fa9d8c76dc9c4c
llynn <lexielynn1@gmail.com>
2014-11-18 20:52:16 +0100

    Initial Commit

---
+++ ZephyrWeatherAppTests/ZephyrWeatherAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  ZephyrWeatherAppTests.swift
+//  ZephyrWeatherAppTests
+//
+//  Created by A. Lynn on 11/18/14.
+//  Copyright (c) 2014 Lexie Lynn. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ZephyrWeatherAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
